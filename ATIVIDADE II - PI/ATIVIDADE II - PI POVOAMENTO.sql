e3fa445f126c20e5767ace78ec503cb41a835621
HumbertoJ-r <125837688+HumbertoJ-r@users.noreply.github.com>
2023-06-15 21:52:51 -0300

    CONTINUAÇÃO DA ATIVIDADE II - PI

---
+++ ATIVIDADE II - PI/ATIVIDADE II - PI POVOAMENTO.sql
@@ -0,0 +1,5 @@
+use db_nova_belo
+
+insert into CLIENTE (nome, cpf, rg, data_de_nascimento, endereco, nome_pai, nome_mae, telefone, telefone_contato, email)
+values ('RICARDINHO', '325.652.456-45', '14.256.985-7', '2005-06-15', 'RUA DOS BOBOS, N 0', 'RICARDAO', 'RICARDA', '(11)2255-6633', '(11)98855-7766', 'RICARDO123@GMAIL.COM');
+
