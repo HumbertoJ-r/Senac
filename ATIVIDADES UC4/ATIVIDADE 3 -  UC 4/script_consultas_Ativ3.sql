c8f1e5feab052545c20b790d225563d819807478
HumbertoJ-r <125837688+HumbertoJ-r@users.noreply.github.com>
2023-07-10 19:54:58 -0300

    10-07-2023

--- ATIVIDADES UC4/ATIVIDADE 3 -  UC 4/script_consultas_Ativ3.sql
+++ ATIVIDADES UC4/ATIVIDADE 3 -  UC 4/script_consultas_Ativ3.sql
@@ -6,6 +6,7 @@ Ordena-se pela data de venda, as mais recentes primeiro.
 SELECT * FROM venda v, item_venda iv, produto p, cliente c, funcionario f
 WHERE v.id = iv.venda_id AND c.id = v.cliente_id AND p.id = iv.produto_id AND f.id = v.funcionario_id and tipo_pagamento = 'D'
 
+-- Ajuste realizado com JOIN para otmizar e melhorar a pesquisa anterior --
 SELECT v.data, v.valor_total, iv.nome_produto, iv.quantidade, iv.valor_unitario, c.id, c.cpf, c.telefone
 FROM venda v 
 JOIN  item_venda iv ON v.id = iv.venda_id
@@ -13,6 +14,7 @@ JOIN  cliente c ON v.cliente_id = c.id
 WHERE v.tipo_pagamento = 'D'
 ORDER BY v.data DESC;
 
+-- Criação da VIEW para pesquisar sem a necessidade reescrever o código para uma nova pesquisa com os mesmos parâmetros -- 
 CREATE VIEW vendas_dinheiro AS
 SELECT v.data, v.valor_total, iv.nome_produto, iv.quantidade, iv.valor_unitario, c.id, c.cpf, c.telefone
 FROM venda v 
@@ -21,11 +23,14 @@ JOIN  cliente c ON v.cliente_id = c.id
 WHERE v.tipo_pagamento = 'D'
 ORDER BY v.data DESC;
 
+-- Chamando o resultado da VIEW --
 SELECT * FROM vendas_dinheiro;
 
+-- Pesquisa para comparar o tempo de retorno antes da modificação --
 EXPLAIN SELECT * FROM venda v, item_venda iv, produto p, cliente c, funcionario f
 WHERE v.id = iv.venda_id AND c.id = v.cliente_id AND p.id = iv.produto_id AND f.id = v.funcionario_id and tipo_pagamento = 'D'
- 
+
+ -- Pesquisa para comparar o tempo de retorno depois da modificação --
 EXPLAIN SELECT v.data, v.valor_total, iv.nome_produto, iv.quantidade, iv.valor_unitario, c.id, c.cpf, c.telefone
 FROM venda v 
 JOIN  item_venda iv ON v.id = iv.venda_id
@@ -50,12 +55,14 @@ SELECT *
 FROM produto p, item_venda iv, venda v
 WHERE p.id = iv.produto_id AND v.id = iv.venda_id AND p.fabricante like '%lar%'
 
+-- Ajuste realizado com JOIN para otmizar e melhorar a pesquisa anterior --
 SELECT p.nome, p.descricao, p.fabricante, iv.quantidade, v.data
 FROM item_venda iv
 JOIN venda v ON iv.venda_id = v.id
 JOIN produto p ON iv.produto_id = p.id
 ORDER BY p.nome;
 
+-- Criação da VIEW para pesquisar sem a necessidade reescrever o código para uma nova pesquisa com os mesmos parâmetros -- 
 CREATE VIEW  descricao_produto AS
 SELECT p.nome, p.descricao, p.fabricante, iv.quantidade, v.data
 FROM item_venda iv
@@ -63,12 +70,15 @@ JOIN venda v ON iv.venda_id = v.id
 JOIN produto p ON iv.produto_id = p.id
 ORDER BY p.nome;
 
+-- Chamando o resultado da VIEW --
 SELECT * FROM descricao_produto;
 
+-- Pesquisa para comparar o tempo de retorno antes da modificação --
 EXPLAIN SELECT * 
 FROM produto p, item_venda iv, venda v
 WHERE p.id = iv.produto_id AND v.id = iv.venda_id AND p.fabricante like '%lar%'
 
+-- Pesquisa para comparar o tempo de retorno depois da modificação --
 EXPLAIN SELECT p.nome, p.descricao, p.fabricante, iv.quantidade, v.data
 FROM item_venda iv
 JOIN venda v ON iv.venda_id = v.id
@@ -84,7 +94,7 @@ FROM produto p, item_venda iv, venda v, cliente c
 WHERE p.id = iv.produto_id AND v.id = iv.venda_id AND c.id = v.cliente_id /*f.id = v.funcionario_id*/
 GROUP BY c.nome, p.nome
 
-
+-- Criação da VIEW para pesquisar sem a necessidade reescrever o código para uma nova pesquisa com os mesmos parâmetros --
 CREATE view total_venda as 
 SELECT SUM(iv.subtotal * iv.quantidade) AS total, c.nome AS nome_cliente, p.nome AS nome_produto, p.descricao, iv.valor_unitario, iv.quantidade, v.valor_total
 FROM venda v
@@ -93,8 +103,10 @@ JOIN item_venda iv ON iv.venda_id = v.id
 JOIN produto p ON iv.produto_id = p.id
 GROUP BY c.nome, p.nome, p.descricao, iv.valor_unitario, iv.quantidade, v.valor_total;
 
+-- Chamando o resultado da VIEW --
 select * from total_venda;
 
+-- Ajuste realizado com JOIN para otmizar e melhorar a pesquisa anterior --
 SELECT SUM(iv.subtotal * iv.quantidade) AS total, c.nome, p.nome, p.descricao, iv.valor_unitario, iv.quantidade, v.valor_total
 FROM venda v
 JOIN cliente c ON v.cliente_id = c.id
@@ -102,11 +114,13 @@ JOIN item_venda iv ON iv.venda_id = v.id
 JOIN produto p ON iv.produto_id = p.id
 GROUP BY c.nome, p.nome, p.descricao, iv.valor_unitario, iv.quantidade, v.valor_total;
 
+-- Pesquisa para comparar o tempo de retorno antes da modificação --
 EXPLAIN SELECT SUM(iv.subtotal), SUM(iv.quantidade)
 FROM produto p, item_venda iv, venda v, cliente c
 WHERE p.id = iv.produto_id AND v.id = iv.venda_id AND c.id = v.cliente_id /*f.id = v.funcionario_id*/
 GROUP BY c.nome, p.nome
 
+-- Pesquisa para comparar o tempo de retorno depois da modificação --
 EXPLAIN SELECT SUM(iv.subtotal * iv.quantidade) AS total, c.nome, p.nome, p.descricao, iv.valor_unitario, iv.quantidade, v.valor_total
 FROM venda v
 JOIN cliente c ON v.cliente_id = c.id
