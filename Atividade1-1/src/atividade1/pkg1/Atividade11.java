65c66427064e4d8947f39d0442d5b3de63b9cee4
HumbertoJ-r <125837688+HumbertoJ-r@users.noreply.github.com>
2023-07-29 14:36:36 -0300

    29-07

--- Atividade1-1/src/atividade1/pkg1/Atividade11.java
+++ Atividade1-1/src/atividade1/pkg1/Atividade11.java
@@ -1,41 +1,43 @@
 package atividade1.pkg1;
+
+import java.util.Scanner;
+
 public class Atividade11 {
     public static void main(String[] args)
     {
-        int alfa = 150;
-        int beta = 350;
-        int cadeiras;
-        int convidados;
+        int Alfa = 150;
+        int Beta = 350;
+        int Convidados;
+        int Cadeiras;
         
-        Scanner scanner = new Scanner(System.in);
-		
+        Scanner teclado = new Scanner (System.in);
 	System.out.println("Digite aqui o número de convidados: ");
-	convidados = scanner.nextInt();
-	System.out.println(convidados + "número de convidados");
+        Convidados = teclado.nextInt();
+	System.out.println(Convidados + "número de convidados");
 
-	if (convidados > 350)
+	if (Convidados > 350)
 		{
                     System.out.println("Número de convidados inválido.");	
 		}
 		
-        else if (convidados < 0)
+        else if (Convidados < 0)
 		{
                     System.out.println("Número de convidados inválido.");
 		}
 
-        else if	(convidados > 220)
+        else if	(Convidados > 220)
 		{
                     System.out.println("Use o auditório Beta.");
 		}
         
-        else if	(convidados < 220)
+        else if	(Convidados < 220)
 		{
                     System.out.println("Use o auditório Alfa.");
 			
-			if (convidados > 150)
+			if (Convidados > 150)
 			{
-			cadeiras = convidados - 150;
-			System.out.println("Inclua mais " + cadeiras + " cadeiras");
+			Cadeiras = Convidados - 150;
+			System.out.println("Inclua mais " + Cadeiras + " cadeiras");
 			}
     }
     }
