ba7845ff3c32364f729561f44c3c5103df54ec3a
HumbertoJ-r <125837688+HumbertoJ-r@users.noreply.github.com>
2023-08-01 19:51:21 -0300

    01-08

--- Aluno/src/aluno/Aluno.java
+++ Aluno/src/aluno/Aluno.java
@@ -10,6 +10,22 @@ public class Aluno {
     float nota2;
     float media;
     
+    public float mostrarMaior(){
+        if (nota1 >= nota2){
+            return nota1;
+        }else{
+            return nota2;
+        }
+    }
+    
+    public void mostrarResultado(){
+        if (media >= 7){
+            System.out.println("O aluno foi aprovado.");
+        } else{
+            System.out.println("O aluno foi reprovado.");
+        }
+    }
+    
     public void descrever2(){
         System.out.println("Nome do Aluno: " + nome);
         System.out.println("Nota 1: " + nota1);
@@ -19,7 +35,7 @@ public class Aluno {
     }
     
     public float calcularMedia(float num1, float num2){
-        return (num1 + num2)/2;
+        return media = (num1 + num2)/2;
     }
     
     public void descrever(){
@@ -30,15 +46,72 @@ public class Aluno {
         System.out.println();
     }
     
-public void mostrarNome(){
+    public void mostrarNome(){
     System.out.println("O aluno se chama: " + nome);
 }
  
     public static void main(String[] args) {
+      Aluno aluno1 = new Aluno();
+      Aluno aluno2 = new Aluno();
+      
+      aluno1.nome = "Maria";
+      aluno2.nome = "João";
+              
+      System.out.println("Aluno1: " + aluno1.nome + " \nAluno2: " + aluno2.nome + "\n");
+      
+      aluno1 = aluno2;
+      
+      System.out.println("Aluno1: " + aluno1.nome + " \nAluno2: " + aluno2.nome + "\n");
+      
+      aluno1.nome = "Severino";
+      
+      System.out.println("Aluno1: " + aluno1.nome + " \nAluno2: " + aluno2.nome);
+      /*
+      Aluno[] aluno = new Aluno[40];
+      int i;
+      
+      for(i = 0; i<=39; i++){
+         aluno[i] = new Aluno();
+      }
+        
+      aluno[0].nome="Maria";
+      aluno[0].descrever2();
+      
+      aluno[20].nome="Jose";
+      aluno[20].descrever2();
+      
+      aluno[39].nome="Josi";
+      aluno[39].descrever2();
+        
+        
        Aluno aluno1 = new Aluno();
        Aluno aluno2 = new Aluno();
        Aluno aluno3 = new Aluno();
        
+       aluno1.nome = "Celso Medeiros";
+       aluno1.nota1 = (float) 9.7;
+       aluno1.nota2 = (float) 6.5;
+      
+       aluno2.nome = "Camila Alves";
+       aluno2.nota1 = (float) 4.2;
+       aluno2.nota2 = (float) 6.5;
+      
+       System.out.println("Mostrar Alunos:");
+       
+       aluno1.descrever2();
+       System.out.println("A média do aluno é: " + aluno1.calcularMedia(aluno1.nota1, aluno1.nota2));
+       System.out.println("A maior nota do aluno é: " + aluno1.mostrarMaior());
+       aluno1.mostrarResultado();
+       System.out.println();
+       
+       aluno2.descrever2();
+       System.out.println("A média do aluino é: " + aluno2.calcularMedia(aluno2.nota1, aluno2.nota2));
+       System.out.println("A maior nota do aluno é: " + aluno2.mostrarMaior());
+       aluno2.mostrarResultado();
+       
+          
+       
+      
        aluno1.nome = "Celso Medeiros";
        aluno1.nota1 = (float) 9.7;
        aluno1.nota2 = (float) 6.5;
@@ -90,8 +163,7 @@ public void mostrarNome(){
        System.out.println("Marticula do aluno3: " + aluno3.matricula);
        System.out.println("CPF do aluno3: " + aluno3.cpf);
        System.out.println("Idade aluno3: " + aluno3.idade);
-       
-       
+       */
     }
     
 }
