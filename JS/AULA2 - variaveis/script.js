e6f4031c5da7c7c5ff33021968db18bef1e804d3
HumbertoJ-r <125837688+HumbertoJ-r@users.noreply.github.com>
2023-09-28 18:44:49 -0300

    28/09

---
+++ JS/AULA2 - variaveis/script.js
@@ -0,0 +1,15 @@
+// tipos primitivos 
+
+// boolean
+var vf = false;
+console.log(typeof(vf));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = "Junior";
+console.log(typeof(nome));
+
+
---
+++ JS/AULA3 - VETORES/script.js
@@ -0,0 +1,5 @@
+// o que são vetores ou array
+
+// como declarar um array
+let array = ['string', 1, true];
+console.log(array);
