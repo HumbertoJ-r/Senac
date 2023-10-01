99f73a11624e40f61346f98b3da135788a11ac76
Humberto junior <125837688+HumbertoJ-r@users.noreply.github.com>
2023-10-01 10:28:03 -0300

    01-10

--- RH/src/rh/RH.java
+++ RH/src/rh/RH.java
@@ -7,9 +7,9 @@ public class RH {
     
     public static void main(String[] args) {
     
-        Funcionarios[] listaDeFun = new Funcionarios [3];
         
-        listaDeFun[0] = new Assalariados ("Nome", "Cpf", "Endereco", "Telefone");
+        
+        
         
     }
     
