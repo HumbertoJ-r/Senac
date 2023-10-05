213b0ea69505fbeeefb99cd0574d9476025e41bf
HumbertoJ-r <125837688+HumbertoJ-r@users.noreply.github.com>
2023-10-05 12:10:43 -0300

    05-10-2023

--- JS/AULA3 - VETORES/script.js
+++ JS/AULA3 - VETORES/script.js
@@ -1,5 +1,15 @@
 // o que são vetores ou array
 
 // como declarar um array
-let array = ['string', 1, true];
-console.log(array);
+//let array = ['string', 1, true];
+//console.log(array);
+
+let array = ['stgring', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(array[3]);
+
+// forEach
+//array.forEach(function(item, index){console.log(item, index)});
+
+// push
+array.push('novo item');
+console.log(array)
