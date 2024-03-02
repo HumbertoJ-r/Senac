c744ef4eaf00a64176d563508ca682fbafc10ec6
HumbertoJ-r <125837688+HumbertoJ-r@users.noreply.github.com>
2024-03-02 13:12:28 -0300

    02-03

---
+++ UC'S MATERIAIS/PI/ATIVIDADE PI - PART 2/ATIVIDADE PI PAT 2 - II/NovaBelo/src/novabelo/ListaCliente.java
@@ -0,0 +1,14 @@
+
+package novabelo;
+
+import java.util.ArrayList;
+
+public class ListaCliente {
+
+    public static ArrayList <Cliente> lista = new ArrayList <Cliente>();
+    
+    public static ArrayList <Cliente> listar () {
+        return lista;
+    }
+    
+}
